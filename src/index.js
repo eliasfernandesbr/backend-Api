7ae56b98ff2cd1ec4be48553789734b1caee2ff9
eliasfernandesbr <eliasfernandescoutinho@gmail.com>
2021-03-24 21:34:23 -0300

    Finalizando estudos

--- src/index.js
+++ src/index.js
@@ -1,67 +1,93 @@
 const { response, json } = require("express");
 const express = require("express");
-const { v4: uuidv4 } = require('uuid');
-uuidv4();
+const { v4: uuidv4 } = require("uuid");
+// const {uuid, isUuid } = require("uuidv4");
+const { validate: uuidValidate } = require("uuid");
 
 const app = express();
 app.use(express.json());
 
 const projects = [];
 
-app.get("/projects", (request, response) => {
-  const {title} = request.query;
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+  console.log("PASSO 1");
+  console.time(logLabel);
+
+  next();
+
+  console.log("PASSO 2");
+  console.timeEnd(logLabel);
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if (!uuidValidate(id)) {
+    return response.status(400).json({ error: "Invalid Project ID" });
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+
+app.get("/projects", validateProjectId, (request, response) => {
+  console.log("PASSO 3");
+  const { title } = request.query;
   const results = title
-  ? projects.filter(project => project.title.includes(title))
-  : projects;
-    
+    ? projects.filter((project) => project.title.includes(title))
+    : projects;
 
   return response.json(projects);
 });
 
-app.post("/projects", (request, response) => {
-    const {title, owner} = request.body;
-    
-    const project = { id: uuidv4(), title, owner};
-    projects.push(project)
+app.post("/projects", validateProjectId, (request, response) => {
+  const { title, owner } = request.body;
 
-  return response.json(project)
+  const project = { id: uuidv4(), title, owner };
+  console.log(project);
+  projects.push(project);
+
+  return response.json(project);
 });
 
-app.put("/projects/:id", (request, response) => {
-    const {id} = request.params;
-    const { title, owner} = request.body;
-    
-    const projectIndex = projects.findIndex(project => project.id === id)
+app.put("/projects/:id", validateProjectId, (request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
 
-    if(projectIndex < 0){
-      return response.status(400).json({error: "Project Not Found"})
-    }
+  const projectIndex = projects.findIndex((project) => project.id === id);
 
-    const project  = {
-      id,
-      title,
-      owner,
-    };
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project Not Found" });
+  }
 
-    projects[projectIndex] =  project;
+  const project = {
+    id,
+    title,
+    owner,
+  };
 
+  projects[projectIndex] = project;
 
-    return response.json(project)
-  });
+  return response.json(project);
+});
 
-  app.delete("/projects/:id", (request, response) => {
-    const {id} = request.params;
+app.delete("/projects/:id", validateProjectId, (request, response) => {
+  const { id } = request.params;
 
-    const projectIndex = projects.findIndex(project => project.id === id)
+  const projectIndex = projects.findIndex((project) => project.id === id);
 
-    if(projectIndex < 0){
-      return response.status(400).json({error: "Project Not Found"})
-    }
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project Not Found" });
+  }
 
-    projects.splice(projectIndex, 1);
+  projects.splice(projectIndex, 1);
 
-    return response.status(204).send();
-  });
+  return response.status(204).send();
+});
 
 app.listen(3000, () => {
   console.log("🔶BACK-END STARTED");
